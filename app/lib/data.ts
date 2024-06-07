5db22f8a823b2bf4a95004078493fe0cff7d5d24
Juli <julianalicode@gmail.com>
2024-06-07 15:01:37 -0400

    add login

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,17 +9,18 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
+import {unstable_noStore as noStore} from 'next/cache'
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-
+  noStore() //dynamic rendings
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+     console.log('Fetching revenue data...');
+     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
@@ -33,6 +34,8 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+    noStore()
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -53,6 +56,8 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+    noStore()
+
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -63,7 +68,7 @@ export async function fetchCardData() {
          SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
          SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
          FROM invoices`;
-
+    //A common way to avoid waterfalls is to initiate all data requests at the same time - in parallel.
     const data = await Promise.all([
       invoiceCountPromise,
       customerCountPromise,
@@ -92,6 +97,8 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number,
 ) {
+    noStore()
+
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -124,6 +131,8 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
+    noStore()
+
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -145,6 +154,8 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+    noStore()
+
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -188,6 +199,8 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
+    noStore()
+
   try {
     const data = await sql<CustomersTableType>`
 		SELECT
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,30 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
-export default function Search({ placeholder }: { placeholder: string }) {
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    // URLSearchParams is a Web API that provides utility methods for manipulating the URL query parameters. Instead of creating a complex string literal, you can use it to get the params string like ?page=1&query=a.
+    if (term) {
+      params.set('query', term);
+      //Next, set the params string based on the user’s input. If the input is empty, you want to delete it:
+    } else {
+      params.delete('query');
+    }
+    console.log(term);
+    replace(`${pathname}?${params.toString()}`);
+    //${pathname} is the current path, in your case, "/dashboard/invoices".
+    //As the user types into the search bar, params.toString() translates this input into a URL-friendly format.
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -10,7 +32,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+        //To ensure the input field is in sync with the URL and will be populated when sharing, you can pass a defaultValue to input by reading from searchParams:
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
